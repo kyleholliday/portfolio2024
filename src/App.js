eca96c5a36c155904bed54b43aafb84e84eac95d
Kyle Holliday <kholliday88@gmail.com>
2024-03-17 22:26:19 -0400

    removing spotify for the time being

--- src/App.js
+++ src/App.js
@@ -827,7 +827,7 @@ export default function App() {
                       />
                     </div>
                   </li>
-                  <li className="mb-12">
+                  {/* <li className="mb-12">
                     <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                       <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-200/60 dark:lg:group-hover:dark:bg-zinc-700/50"></div>
                       <div className="z-10 sm:order-2 sm:col-span-4">
@@ -878,7 +878,7 @@ export default function App() {
                         src="spotify-profile.png"
                       />
                     </div>
-                  </li>
+                  </li> */}
                   {/* <li className="mb-12">
                     <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                       <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-200/60 dark:lg:group-hover:dark:bg-zinc-700/50"></div>
