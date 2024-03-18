479e0015c9277f87fecd6ae92a19c86c156728d7
Kyle Holliday <kholliday88@gmail.com>
2024-03-17 20:42:48 -0400

    adding my spotify project

--- src/App.js
+++ src/App.js
@@ -827,6 +827,58 @@ export default function App() {
                       />
                     </div>
                   </li>
+                  <li className="mb-12">
+                    <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-200/60 dark:lg:group-hover:dark:bg-zinc-700/50"></div>
+                      <div className="z-10 sm:order-2 sm:col-span-4">
+                        <h3>
+                          <a
+                            className="inline-flex items-baseline font-medium leading-tight text-slate-900 dark:text-gray-200 dark:hover:text-green-500 dark:focus-visible:text-green-500 hover:text-blue-500 focus-visible:text-blue-500 group/link text-base"
+                            href="https://holliday-spotify-app-1268cb18a08c.herokuapp.com/"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="Bijou Movies and TV Database"
+                          >
+                            <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                            <span>
+                              Bijou{' '}
+                              <span className="inline-block">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                  className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none"
+                                  aria-hidden="true"
+                                >
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+                                    clipRule="evenodd"
+                                  ></path>
+                                </svg>
+                              </span>
+                            </span>
+                          </a>
+                        </h3>
+                        <p className="mt-2 text-sm leading-normal">
+                          Application that uses Spotify's API, React, and
+                          Node.js to show your profile, top artists, top tracks,
+                          and some other fun stuff. You'll need a Spotify login
+                          to check it out.
+                        </p>
+                      </div>
+                      <img
+                        alt=""
+                        loading="lazy"
+                        width="300"
+                        decoding="async"
+                        data-nimg="1"
+                        className="rounded border-2 border-gray-200/10 transition group-hover:border-gray-200/30 sm:order-1 sm:col-span-4"
+                        srcSet="spotify-profile.png"
+                        src="spotify-profile.png"
+                      />
+                    </div>
+                  </li>
                   {/* <li className="mb-12">
                     <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                       <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-200/60 dark:lg:group-hover:dark:bg-zinc-700/50"></div>
