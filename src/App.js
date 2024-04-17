1115a6904f77904ac3177480a604e16589fc37e3
Kyle Holliday <kholliday88@gmail.com>
2024-04-16 21:15:54 -0400

    small text edit

--- src/App.js
+++ src/App.js
@@ -200,8 +200,8 @@ export default function App() {
                   for the better part of a decade. My knowledge mainly focuses
                   on crafting seamless user experiences using React, Angular,
                   and JavaScript. While coding makes up the majority of my
-                  portfolio, I also enjoying designing through using tools like
-                  Figma and Sketch.
+                  portfolio, I also enjoying designing with tools like Figma and
+                  Sketch.
                 </p>
                 <p className="mb-4">
                   Throughout my career, I've had the privilege of contributing
